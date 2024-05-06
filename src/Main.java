6560892b06bb8d259924203ac59cf5000aede689
W Cardoso <cardosu7@gmail.com>
2024-05-06 13:59:08 -0300

    Numeros positivos

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ public class Main {
 
         OrdemNumerica ordemNumerica = new OrdemNumerica();
         NumerosPares numerosPares = new NumerosPares();
+        NumerosPositivos numerosPositivos = new NumerosPositivos();
 
         System.out.println(ordemNumerica.getNumerosOrdenados());
 
         System.out.println(numerosPares.getNumerosPares());
+
+        System.out.println(numerosPositivos.getNumerosPositivos());
 
     }
 }
